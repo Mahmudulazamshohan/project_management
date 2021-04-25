12e822b02b1c379b20f2bc34eccea413fdd459b8
DESKTOP-JS6N0RP\User <mazam@cisscom.com>
2021-04-25 23:56:08 +0600

    dialog added

--- src/components/board/board.list.dialog.tsx
+++ src/components/board/board.list.dialog.tsx
@@ -1,11 +1,49 @@
-import { Col, Modal, Row } from "antd";
-import TextArea from "antd/lib/input/TextArea";
+import { faBug, faLink, faPaperclip } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useCallback, useMemo, useRef, useState } from "react";
+import { Col, Modal, Row, Button } from "antd";
+// const RichTextExample = () => {
+//   const [value, setValue] = useState<Descendant[]>(initialValue)
+//   const renderElement = useCallback(props => <Element {...props} />, [])
+//   const renderLeaf = useCallback(props => <Leaf {...props} />, [])
+//   const editor = useMemo(() => withHistory(withReact(createEditor())), [])
+
+//   return (
+//     <Slate editor={editor} value={value} onChange={value => setValue(value)}>
+      
+//       <Editable
+//         renderElement={renderElement}
+//         renderLeaf={renderLeaf}
+//         placeholder="Enter some rich text…"
+//         spellCheck
+//         autoFocus
+//         onKeyDown={event => {
+//           for (const hotkey in HOTKEYS) {
+//             if (isHotkey(hotkey, event as any)) {
+//               event.preventDefault()
+//               const mark = HOTKEYS[hotkey]
+//               toggleMark(editor, mark)
+//             }
+//           }
+//         }}
+//       />
+//     </Slate>
+//   )
+// }
+
+
 interface IBoardListDialog {
   isVisible: boolean;
   onCancel?: (e: React.MouseEvent<HTMLElement>) => void;
   onOk?: (e: React.MouseEvent<HTMLElement>) => void;
 }
 const BoardListDialog = ({ isVisible, onCancel, onOk }: IBoardListDialog) => {
+  const editor = useRef<HTMLDivElement>(null);
+  const [content, setContent] = useState("");
+
+  const config = {
+    readonly: false, // all options from https://xdsoft.net/jodit/doc/
+  };
   return (
     <Modal
       title="Drag and drop issue"
@@ -18,8 +56,22 @@ const BoardListDialog = ({ isVisible, onCancel, onOk }: IBoardListDialog) => {
     >
       <Row>
         <Col span={16}>
-          <p>asdasdas</p>
-          <TextArea />
+          <div className="actions">
+            <Button
+              type="default"
+              icon={<FontAwesomeIcon icon={faPaperclip} />}
+            >
+              Attach
+            </Button>
+            <Button type="default" icon={<FontAwesomeIcon icon={faBug} />}>
+              Add a child issue
+            </Button>
+            <Button type="default" icon={<FontAwesomeIcon icon={faLink} />}>
+              Link Issue
+            </Button>
+          </div>
+
+          
         </Col>
         <Col span={8}>
           <p>asdasdas</p>
